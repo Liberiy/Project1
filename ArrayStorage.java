6142bef31f8085feb9d7459433d80b699df7ebb5
Golovin Maksim <dp280191gms@gmail.com>
2016-06-23 06:35:15 +0000

    Add files via upload

--- ArrayStorage.java
+++ ArrayStorage.java
@@ -13,6 +13,23 @@ public class ArrayStorage extends AbstractArrayStorage {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
+    public void Sort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
+    }
 
     public void update(Resume r) {
         int index = getIndex(r.getUuid());
